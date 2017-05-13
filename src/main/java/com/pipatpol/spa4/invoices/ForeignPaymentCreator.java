01ca94be0d4958d16fa1217ecdb0fa80a8f3eefb
PNNutkung <pipatpol.t@ku.th>
2017-05-13 13:23:19 +0700

    Add foriegn payment creator

---
+++ src/main/java/com/pipatpol/spa4/invoices/ForeignPaymentCreator.java
@@ -0,0 +1,24 @@
+package com.pipatpol.spa4.invoices;
+
+import com.pipatpol.spa4.banking.Payment;
+import com.pipatpol.spa4.banking.PaymentCreator;
+import com.pipatpol.spa4.banking.PaymentException;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by PNNutkung on 2017-05-13.
+ */
+@Component
+public class ForeignPaymentCreator implements PaymentCreator {
+
+    public static final String CURRENT_IBAN_ACC = "current-iban-acc";
+    @Override
+    @Transformer
+    public Payment createPayment(Invoice invoice) throws PaymentException {
+        if (null == invoice.getIban()) {
+            throw new PaymentException("IBAN mustn't be null when creating foreign payment!");
+        }
+        return new Payment(CURRENT_IBAN_ACC, invoice.getIban(), invoice.getDollars());
+    }
+}
