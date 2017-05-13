3a5218a8ffa8a58d8f43aeada0702a0f7cea5f34
PNNutkung <pipatpol.t@ku.th>
2017-05-13 13:16:12 +0700

    Add local payment creator

---
+++ src/main/java/com/pipatpol/spa4/invoices/LocalPaymentCreator.java
@@ -0,0 +1,25 @@
+package com.pipatpol.spa4.invoices;
+
+import com.pipatpol.spa4.banking.Payment;
+import com.pipatpol.spa4.banking.PaymentCreator;
+import com.pipatpol.spa4.banking.PaymentException;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by PNNutkung on 2017-05-13.
+ */
+@Component
+public class LocalPaymentCreator implements PaymentCreator {
+
+    private static final String CURRENT_LOCAL_ACC = "current-local-acc";
+
+    @Override
+    @Transformer
+    public Payment createPayment(Invoice invoice) throws PaymentException {
+        if (null == invoice.getAccount()) {
+            throw new PaymentException("Account can not be empty when creating local payment!");
+        }
+        return new Payment(CURRENT_LOCAL_ACC, invoice.getAccount(), invoice.getDollars());
+    }
+}
