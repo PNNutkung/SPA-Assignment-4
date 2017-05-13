8eaefa10f8c21b884b41cc5f16b1c7b345f006a4
PNNutkung <pipatpol.t@ku.th>
2017-05-13 11:36:38 +0700

    Add invoices schema

--- src/main/java/com/pipatpol/spa4/invoices/InvoiceCollectorGateway.java
+++ src/main/java/com/pipatpol/spa4/invoices/InvoiceCollectorGateway.java
@@ -1,7 +1,10 @@
 package com.pipatpol.spa4.invoices;
 
+import java.util.Collection;
+
 /**
  * Created by PNNutkung on 2017-05-13.
  */
 public interface InvoiceCollectorGateway {
+    void collectInvoices(Collection<Invoice> invoices);
 }
